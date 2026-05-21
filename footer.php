d2522118cce3ceeeec7c9629482d10815409596a
Argjend05 <argjendkallari@gmail.com>
2026-05-21 14:40:12 +0200

    .

--- footer.php
+++ footer.php
@@ -96,7 +96,6 @@ endif;
                 <?php elseif (is_page_template('page-outils-terrain.php')): ?>
                     <li><a href="<?php echo esc_url(home_url('/')); ?>">Accueil</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('professionnel'))); ?>">Espace Professionnels</a></li>
-                    <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('temoignages'))); ?>">Témoignages</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('formulaire-contact'))); ?>">Contact</a></li>
                 <?php elseif (is_page_template('page-formulaire-outil.php')):
                     $_ot_pages = get_posts([
@@ -112,12 +111,15 @@ endif;
                     <?php endif; ?>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('professionnels'))); ?>">Espace Professionnels</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('formulaire-contact'))); ?>">Contact</a></li>
+                <?php elseif (is_page_template('page-observatoire.php')): ?>
+                    <li><a href="<?php echo esc_url(home_url('/')); ?>">Accueil</a></li>
+                    <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('professionnels'))); ?>">Espace Professionnels</a></li>
+                    <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('formulaire-contact'))); ?>">Contact</a></li>
                 <?php else: ?>
                     <!-- Navigation générique — pages sans section d'ancres dédiée -->
                     <li><a href="<?php echo esc_url(home_url('/')); ?>">Accueil</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('professionnels'))); ?>">Espace Professionnels</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('observatoire'))); ?>">Observatoire</a></li>
-                    <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('temoignages'))); ?>">Témoignages</a></li>
                     <li><a href="<?php echo esc_url(get_permalink(get_page_by_path('formulaire-contact'))); ?>">Contact</a></li>
                 <?php endif; ?>
             </ul>
