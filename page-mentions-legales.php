f8d2b3cb9c91a666953986edc1599561dbd148e7
Argjend05 <argjendkallari@gmail.com>
2026-05-29 10:55:47 +0200

    .

--- page-mentions-legales.php
+++ page-mentions-legales.php
@@ -256,6 +256,16 @@ get_header();
                     <p class="ml-text-icon">
                         <span class="ml-icon-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16" height="16">
+                                <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
+                            </svg>
+                        </span>
+                        <span>
+                            Site conçu et développé par <strong>Argjend KALLARI</strong>.
+                        </span>
+                    </p>
+                    <p class="ml-text-icon">
+                        <span class="ml-icon-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16" height="16">
                                 <path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z"/>
                             </svg>
                         </span>
